188b0bc2b496ac8abe92bc745f6a6adc2515c1fe
Noemyye <mimisuper48@gmail.com>
2026-06-19 15:07:54 +0200

    tp3

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import { InMemoryCounter } from './InMemoryCounter';    
+import os from 'node:os';
 
 const app = express();
 const port = process.env.PING_LISTEN_PORT || 3000;
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 
 app.get('/ping', (req, res) => {
     countRequest++;
+  console.log(`/ping handled by hostname: ${os.hostname()}`);
     if (req.headers) {
         return res.send(`Header is: ${JSON.stringify(req.headers)}`);
     } else {
